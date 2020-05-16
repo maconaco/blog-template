ac5a022c572a7353154fd5268095738c84cbd549
maconaco <makomakopg@gmail.com>
2020-05-16 22:10:45 +0900

    Add editorconfig setting.

--- src/components/ArticleContent.tsx
+++ src/components/ArticleContent.tsx
@@ -1,13 +1,11 @@
-import * as React from 'react';
+import * as React from "react";
 
-import { Article } from '../types';
+import { Article } from "../types";
 
 type Props = {
-  articleContent: Article['content'];
+    articleContent: Article["content"];
 };
 
 export default ({ articleContent }: Props) => {
-  return (
-    <h1>{articleContent}</h1>
-  );
+    return <h1>{articleContent}</h1>;
 };
