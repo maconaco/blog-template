04f06d519b1d5b912e2623444931c293b45d161e
maconaco <makomakopg@gmail.com>
2020-05-16 22:07:31 +0900

    Add editorconfig file.

---
+++ src/components/ArticleContent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import { Article } from '../types';
+
+type Props = {
+  articleContent: Article['content'];
+};
+
+export default ({ articleContent }: Props) => {
+  return (
+    <h1>{articleContent}</h1>
+  );
+};
